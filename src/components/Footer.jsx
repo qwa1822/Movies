e86d32cbbf54119e314048d3fb882c38c0ab2b09
qwa1822 <asdzxc1822@naver.com>
2024-03-18 10:43:58 +0900

    'edit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
          p-4 md:flex md:items-center md:justify-between"
         >
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            © 2023{" "}
+            © 2024{" "}
             <a href="/" className="hover:underline">
               Movies
             </a>
